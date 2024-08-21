713f8b187d863cabb48259151f3682fd20ffcf98
ochab.mat@gmail.com <ochab.mat@gmail.com>
2024-08-21 19:45:30 +0200

    feature/PREG-19 add swagger and describe endpoints

--- src/main/java/com/pregenmed/pgmarticles/infrastructure/article/controller/ArticleController.java
+++ src/main/java/com/pregenmed/pgmarticles/infrastructure/article/controller/ArticleController.java
@@ -3,6 +3,9 @@ package com.pregenmed.pgmarticles.infrastructure.article.controller;
 import com.pregenmed.pgmarticles.infrastructure.article.controller.dto.request.AddArticleRequest;
 import com.pregenmed.pgmarticles.infrastructure.article.controller.dto.response.AddArticleResponse;
 import com.pregenmed.pgmarticles.infrastructure.article.controller.dto.response.GetArticleByUuidResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +13,30 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/articles")
+@Tag(name = "Article")
 public interface ArticleController {
 
-    //TODO add swagger
     //TODO add javadoc
-
+    @Operation(
+            description = "Give me article UUID and I will return you article",
+            summary = "Get article",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200")
+            }
+    )
     GetArticleByUuidResponse getArticleByUuid(UUID uuid);
 
+    @Operation(
+            description = "Give me valid article and I will store it",
+            summary = "Add article",
+            responses = {
+                    @ApiResponse(
+                            description = "Created",
+                            responseCode = "1")
+            }
+    )
     AddArticleResponse addArticle(AddArticleRequest addArticleRequest);
 
 
